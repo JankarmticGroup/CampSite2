3db53355f105ead93efa16ce0ac211a5d662400c
Jankarmtic <jankarmticgroup@gmail.com>
2021-05-20 11:51:40 -0400

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,15 +1,18 @@
 
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
 function RenderDirectoryitem({campsite}){
     return (
         <Card >
-        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardImgOverlay>
-            <CardTitle>{campsite.name}</CardTitle>
-        </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 };
